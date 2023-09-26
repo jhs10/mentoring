12e2e2102a477df87f28ec646a033b08661d49cb
jhs10 <144254838+jhs10@users.noreply.github.com>
2023-09-26 20:08:09 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	printf("");
+
+	for (int i=1; i<=N; i++)
+	{
+		printf("%d\n", i);
+	}
+}
